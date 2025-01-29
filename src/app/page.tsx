0b447a24bbd0d6cf3407ac57434a4207b706553b
Matheus Corrêa <mcorreadesign@gmail.com>
2025-01-29 15:33:52 -0300

    feat: Add Material UI components to the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import { Box, Button, TextField } from "@mui/material";
+
 export default function Home() {
   return (
     <div>
       <h1>SuperAdmin</h1>
+      <Box display={"flex"} flexDirection={"column"} alignItems={"center"}>
+        <TextField label="Material Text Field" />
+        <Box m={2}>
+          <Button variant="contained" color="primary">
+            Material Button
+          </Button>
+        </Box>
+      </Box>
     </div>
   );
 }
