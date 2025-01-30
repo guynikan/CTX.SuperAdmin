2d31eeb70cb674e05f0bd989e0a095bb36c7247b
Matheus Corrêa <mcorreadesign@gmail.com>
2025-01-30 10:34:21 -0300

    feat: Integrate React Query for data fetching and add mutation functionality in the homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,18 @@
+"use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const queryClient = new QueryClient();
+
   return (
     <html lang="en">
-      <body>{children}</body>
+      <QueryClientProvider client={queryClient}>
+        <body>{children}</body>
+      </QueryClientProvider>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,93 @@
-import { Box, Button, TextField } from "@mui/material";
+"use client";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CircularProgress,
+  TextField,
+} from "@mui/material";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+interface Pokemon {
+  name: string;
+  url: string;
+}
 
+interface PokemonApiResponse {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: Pokemon[];
+}
 export default function Home() {
+  const queryClient = useQueryClient();
+
+  // Queries
+  const { data } = useQuery({ queryKey: ["test"], queryFn: get });
+
+  async function get(): Promise<PokemonApiResponse> {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const json = await response.json();
+    return JSON.parse(JSON.stringify(json));
+  }
+
+  // Mutations
+  const mutation = useMutation({
+    mutationFn: post,
+    onSuccess: () => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries({ queryKey: ["test"] });
+    },
+  });
+
+  async function post() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({ id: Math.random(), title: "New Todo" });
+      }, 1000);
+    });
+  }
+
   return (
     <div>
       <h1>SuperAdmin</h1>
       <Box display={"flex"} flexDirection={"column"} alignItems={"center"}>
         <TextField label="Material Text Field" />
         <Box m={2}>
-          <Button variant="contained" color="primary">
-            Material Button
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => mutation.mutate()}
+            sx={{ marginRight: 2 }}
+          >
+            Mutate
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() =>
+              queryClient.invalidateQueries({ queryKey: ["test"] })
+            }
+          >
+            Invalidate Query
           </Button>
         </Box>
+        {data?.results ? (
+          <Box>
+            {data.results.map((item: Pokemon) => (
+              <Box key={item.name} p={1} m={1}>
+                <Card>
+                  <CardContent>{item.name}</CardContent>
+                </Card>
+              </Box>
+            ))}
+          </Box>
+        ) : (
+          <CircularProgress />
+        )}
       </Box>
     </div>
   );
