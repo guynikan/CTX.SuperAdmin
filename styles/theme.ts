0c976698c24554890b06d44c4d28c5a1bcf4d810
Lincoli Xavier <hello@lincolixavier.com>
2025-02-10 13:10:12 +0700

    feat: add theme styles

---
+++ styles/theme.ts
@@ -0,0 +1,12 @@
+export const theme = {
+  colors: {
+    background: "#f8f8f8",
+    primary: "#000",
+    secondary: "#555",
+    inputBackground: "#fff",
+    inputBorder: "#ccc",
+  },
+  fonts: {
+    regular: "Arial, sans-serif",
+  },
+};
